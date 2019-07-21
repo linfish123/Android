d099a64d02d1f24c1949b3e7274ae64d3377359c
Drake <Drake@DrakedeMBP.r1001.tciii.org.tw>
2019-07-21 11:13:55 +0800

    20190721

---
+++ app/src/main/java/tw/org/iii/apps/drake01/MainActivity.java
@@ -0,0 +1,14 @@
+package tw.org.iii.apps.drake01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
